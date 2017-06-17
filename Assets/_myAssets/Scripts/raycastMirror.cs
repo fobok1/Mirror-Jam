e8bb0e772e506944b19a33e70950a2b78febfbe7
DESKTOP-U6GK099\Fobok <bjbaye@gmail.com>
2017-06-17 03:28:51 -0400

    Added new laser model Added tooltip UI.

--- Assets/_myAssets/Scripts/raycastMirror.cs
+++ Assets/_myAssets/Scripts/raycastMirror.cs
@@ -15,36 +15,61 @@ public class raycastMirror : MonoBehaviour {
 	private float laserTime = 5f;
 	public RaycastReflection2 laserScript;
 	public LineRenderer laser;
+	public GameObject mirrorTooltip;
+	public GameObject laserTooltip;
+	public GameObject panel;
+	public Camera cam;
 	#endregion
 
 	#region Start/Update Methods
 
 	// Update is called once per frame
+
+	private void Start()
+	{
+		cam = GetComponentInChildren<Camera>();
+	}
+
 	void Update ()
 	{
 		RaycastHit hit;
-		Vector3 forward = transform.TransformDirection(Vector3.forward) * distance;
+		Vector3 forward = cam.transform.TransformDirection(Vector3.forward) * distance;
 
-		Debug.DrawRay(transform.position, forward, Color.red);
+		Debug.DrawRay(cam.transform.position, forward, Color.red);
 
-		if (Physics.Raycast(transform.position, forward, out hit))
+		if (Physics.Raycast(cam.transform.position, forward, out hit))
 		{
 			if (hit.collider != null)
 			{
 				if (hit.distance < distance)
 				{
 					//Debug.Log(hit.collider.gameObject.name);
-					if (hit.collider.gameObject.name == "Mirror")
+					if (hit.collider.tag == "Mirror")
 					{
+						panel.SetActive(true);
+						mirrorTooltip.SetActive(true);
 						Transform target = hit.collider.gameObject.transform;
 						Rotate(target);
 					}
-					if (hit.collider.gameObject.name == "Laser Emitter")
+					else if (hit.collider.tag == "Laser")
 					{
+						panel.SetActive(true);
+						laserTooltip.SetActive(true);
 						if (Input.GetButtonDown("Activate"))
 						{
 							StartCoroutine(FireLaser());
 						}
+					}
+					else if (hit.collider.tag == "laserTooltip")
+					{
+						panel.SetActive(true);
+						laserTooltip.SetActive(true);
+					}
+					else
+					{
+						laserTooltip.SetActive(false);
+						mirrorTooltip.SetActive(false);
+						panel.SetActive(false);
 					}
 
 
